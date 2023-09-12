fd18eb102f2b06ef9fb870efdf8cdcd40bd19fc6
Abd elrhman Arafa Hassanien <abdoarafa72@yahoo.com>
2023-09-12 20:57:07 +0300

    Timber
    - add timber for logging

--- app/src/main/java/com/example/fakestoreapp/BaseApp.kt
+++ app/src/main/java/com/example/fakestoreapp/BaseApp.kt
@@ -2,6 +2,30 @@ package com.example.fakestoreapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApp : Application()
+class BaseApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree() {
+
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "global_tag $tag", message, t)
+            }
+
+            /*
+            override fun createStackElementTag(element: StackTraceElement): String? {
+                return String.format(
+                    "%s:%s",
+                    element.methodName,
+                    super.createStackElementTag(element)
+                )
+            }
+             */
+        })
+        Timber.d("App Created!")
+    }
+}
